554c28bcf102f001e7be6380aee02f043ec6ea82
Kaeneth Dayao <kendayao@gmail.com>
2020-11-27 13:28:08 -0800

    drop down menu setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,53 @@
+import React, {useState, useEffect} from "react"
+import { FormControl, MenuItem, Select } from '@material-ui/core';
 import './App.css';
 
 function App() {
+
+  const[countries, setCountries]=useState([]);
+  const[country, setCountry]=useState('worldwide');
+
+  useEffect(()=>{
+    const getCountriesData=async()=>{
+      await fetch("https://disease.sh/v3/covid-19/countries")
+      .then((response)=>response.json())
+      .then((data)=>{
+        const countries= data.map((country)=>(
+          {
+            name: country.country,
+            value: country.countryInfo.iso2
+          }));
+
+          setCountries(countries)
+      })
+    }
+    getCountriesData();
+  },[])
+
+  const onCountryChange=(event)=>{
+    const countryCode=event.target.value
+    setCountry(countryCode)
+  }
+
+  
+
   return (
     <div className="app">
-      
+      <div className="app__header">
+        <h1>COVID-19 Tracker</h1>
+        <FormControl className="app-dropdown">
+        <Select
+          variant="outlined"
+          value={country}
+          onChange={onCountryChange}
+        >
+            <MenuItem value="worldwide">Worldwide</MenuItem>
+          {countries.map((country)=>(
+            <MenuItem value={country.value}>{country.name}</MenuItem>
+          ))}
+        </Select>
+        </FormControl>
+      </div>
     </div>
   );
 }
